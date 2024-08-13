ed62dd5329bf2774474349a16bfe2b92f31f68fa
Shaheer-Kashif <codesshaheer@gmail.com>
2024-08-13 11:41:45 +0500

    Base Code Added

---
+++ app.js
@@ -0,0 +1,27 @@
+let turnX = true;
+let buttons = document.querySelectorAll(".box");
+
+const winPatterns = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
+
+
+buttons.forEach( (button) => {
+    button.addEventListener("click", (e) => {
+    if(turnX) {
+        button.innerText = "X";
+        turnX = false;
+    }
+    else {
+        button.innerText = "O";
+        turnX = true;
+    }
+});
+});
